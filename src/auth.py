cc1fe2b721a23b3a079a75a8ff34cc6537dbd377
mike <michael56653556@gmail.com>
2021-10-28 01:37:19 -0400

    user login with tokens generated

--- src/auth.py
+++ src/auth.py
@@ -1,8 +1,9 @@
-from src.constants.http_status_codes import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
-from flask import Blueprint, request
-from flask.json import jsonify
+from os import access
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
+from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 import validators
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 from src.database import User, db
 
@@ -36,20 +37,42 @@ def register():
 
     pwd_hash = generate_password_hash(password)
 
-    user=User(username=username, password=pwd_hash, email=email)
+    user = User(username=username, password=pwd_hash, email=email)
     db.session.add(user)
     db.session.commit()
 
-
     return jsonify({
         'message': 'User created',
-        'user':{
-            'username': username, 
+        'user': {
+            'username': username,
             'email': email
         }
     }), HTTP_201_CREATED
 
 
+@auth.post('/login')
+def login():
+    email = request.json.get('email', '')
+    password = request.json.get('password', '')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        is_pass_correct = check_password_hash(user.password, password)
+
+        if is_pass_correct:
+            refresh = create_refresh_token(identity=user.id)
+            access = create_access_token(identity=user.id)
+
+            return jsonify({
+                'user': {
+                    'refresh': refresh,
+                    'access': access,
+                    'username': user.username,
+                    'email': user.email
+                }
+            }), HTTP_200_OK
+    return jsonify({'error': 'Wrong credentials'}), HTTP_401_UNAUTHORIZED
 
 
 @auth.get("/me")
