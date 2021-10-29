e30c3e91fd16e2dbccb53b2463e3b8671485e9be
mike <michael56653556@gmail.com>
2021-10-29 16:25:56 -0400

    delete one bookmark

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,5 +1,5 @@
 from flask_jwt_extended.view_decorators import jwt_required
-from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
 from flask import Blueprint, request
 from flask.json import jsonify
 import validators
@@ -102,6 +102,20 @@ def get_bookmark(id):
         'updated_at': bookmark.updated_at,
     }), HTTP_200_OK
 
+@bookmarks.delete("/<int:id>")
+@jwt_required()
+def delete_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({'message': 'Item not found'}), HTTP_404_NOT_FOUND
+
+    db.session.delete(bookmark)
+    db.session.commit()
+
+    return jsonify({}), HTTP_204_NO_CONTENT
 
 @bookmarks.put('/<int:id>')
 @bookmarks.patch('/<int:id>')
